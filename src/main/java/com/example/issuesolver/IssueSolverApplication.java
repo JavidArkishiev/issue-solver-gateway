13dc2cd2352736daa58a74e4cd14b9cb39f16a5e
Javid Arkishiev <javidarkishiev@gmail.com>
2024-07-12 16:49:01 +0400

    change port

--- src/main/java/com/example/issuesolver/IssueSolverApplication.java
+++ src/main/java/com/example/issuesolver/IssueSolverApplication.java
@@ -3,11 +3,15 @@ package com.example.issuesolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Collections;
+
 @SpringBootApplication
 public class IssueSolverApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IssueSolverApplication.class, args);
+        SpringApplication app = new SpringApplication(IssueSolverApplication.class);
+        app.setDefaultProperties(Collections.singletonMap("server.port", System.getenv("PORT")));
+        app.run(args);
     }
 
 }
