fc7d5c33191a80ceaf1cb33a03fec890184535d8
Javid Arkishiev <javidarkishiev@gmail.com>
2024-07-16 11:05:29 +0400

    add cors config

--- src/main/java/com/example/issuesolver/CorsConfig.java
+++ src/main/java/com/example/issuesolver/CorsConfig.java
@@ -1,32 +1,32 @@
-package com.example.issuesolver;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.reactive.CorsWebFilter;
-import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
-
-import java.util.Arrays;
-import java.util.Collections;
-
-@Configuration
-public class CorsConfig {
-
-    @Bean
-    public CorsWebFilter corsWebFilter() {
-        CorsConfiguration corsConfig = new CorsConfiguration();
-        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
-        corsConfig.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"));
-        corsConfig.setMaxAge(3600L);
-        corsConfig.setAllowedHeaders(Arrays.asList(
-                "Authorization", "x-xsrf-token", "Accept-language", "Access-Control-Allow-Headers",
-                "Origin", "Accept", "X-Requested-With", "userId",
-                "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers",
-                "Access-Control-Expose-Headers", "X-Session-Id", "X-Platform"));
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", corsConfig);
-
-        return new CorsWebFilter(source);
-    }
-}
+//package com.example.issuesolver;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.cors.CorsConfiguration;
+//import org.springframework.web.cors.reactive.CorsWebFilter;
+//import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+//
+//import java.util.Arrays;
+//import java.util.Collections;
+//
+//@Configuration
+//public class CorsConfig {
+//
+//    @Bean
+//    public CorsWebFilter corsWebFilter() {
+//        CorsConfiguration corsConfig = new CorsConfiguration();
+//        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+//        corsConfig.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"));
+//        corsConfig.setMaxAge(3600L);
+//        corsConfig.setAllowedHeaders(Arrays.asList(
+//                "Authorization", "x-xsrf-token", "Accept-language", "Access-Control-Allow-Headers",
+//                "Origin", "Accept", "X-Requested-With", "userId",
+//                "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers",
+//                "Access-Control-Expose-Headers", "X-Session-Id", "X-Platform"));
+//
+//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//        source.registerCorsConfiguration("/**", corsConfig);
+//
+//        return new CorsWebFilter(source);
+//    }
+//}
--- src/main/java/com/example/issuesolver/GatewayConfig.java
+++ src/main/java/com/example/issuesolver/GatewayConfig.java
@@ -4,6 +4,12 @@ import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 public class GatewayConfig {
@@ -39,5 +45,23 @@ public class GatewayConfig {
                         .filters(f -> f.stripPrefix(2))
                         .uri("https://request-ms-d25203bd24ff.herokuapp.com/"))
                 .build();
+    }
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfig.setAllowedMethods(Arrays.asList("GET", "PUT", "POST", "DELETE", "OPTIONS", "PATCH"));
+        corsConfig.setMaxAge(3600L);
+        corsConfig.setAllowedHeaders(Arrays.asList(
+                "Authorization", "x-xsrf-token", "Accept-language", "Access-Control-Allow-Headers",
+                "Origin", "Accept", "X-Requested-With", "userId",
+                "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers",
+                "Access-Control-Expose-Headers", "X-Session-Id", "X-Platform"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+
+        return new CorsWebFilter(source);
     }
 }
